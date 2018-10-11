bfd4ca141fc4128e82f6ad44175e58e623fbad83
Kunal Sheth <kunal@kunalsheth.info>
2018-10-11 15:00:59 -0700

    Write collector clamp/pivot exercise

--- src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
+++ src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
@@ -1,20 +1,32 @@
 package com.lynbrookrobotics.workshops
 
+import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.Solenoid
 import edu.wpi.first.wpilibj.hal.HAL
 
-// BASE TEMPLATE
+// WEEK ONE
 class FunkyRobot : RobotBase() {
 
     override fun startCompetition() {
 
-        // write initialization code here!
+        // initialization code:
+
+        val pivot = Solenoid(1)
+        val clamp = Solenoid(2)
+
+        val joystick = Joystick(0)
+        val frontTriggerRawButtonId = 1
+        val thumbTriggerRawButtonId = 2
 
         HAL.observeUserProgramStarting()
 
         while (true) {
 
             // write control code here!
+
+            // use the `set` method to open or close a solenoid valve
+            // use the `getRawButton` method to check if a joystick button is pressed
 
             m_ds.waitForData()
         }
