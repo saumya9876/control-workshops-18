b53b1d704348fc40b07b7acdddca59b1cddf2e7c
Kunal Sheth <kunal@kunalsheth.info>
2018-10-11 14:41:17 -0700

    Create FRC robot code template

---
+++ src/main/kotlin/com/lynbrookrobotics/workshops/FunkyRobot.kt
@@ -0,0 +1,22 @@
+package com.lynbrookrobotics.workshops
+
+import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj.hal.HAL
+
+// BASE TEMPLATE
+class FunkyRobot : RobotBase() {
+
+    override fun startCompetition() {
+
+        // write initialization code here!
+
+        HAL.observeUserProgramStarting()
+
+        while (true) {
+
+            // write control code here!
+
+            m_ds.waitForData()
+        }
+    }
+}
